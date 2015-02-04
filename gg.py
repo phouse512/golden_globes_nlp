11812173aea6a8a4517d6719c40bd163a5d728af
phouse512 <philiphouse2015@u.northwestern.edu>
2015-02-03 21:37:26 -0600

    frequency map

--- gg.py
+++ gg.py
@@ -11,6 +11,19 @@ with open(filename) as f:
     for line in f:
         data.append(json.loads(line))
 
+frequency_map = {}
 # array of tweets in data[0]
-for index, text in enumerate(data[0][:10]):
-	print "Current Tweet %s: %s" % (index, text['text'])
+for index, text in enumerate(data[0]):
+	print "Current Tweet %s: %s" % (index, text)
+	word_array = text['text'].split(" ")
+	for word in word_array:
+
+		if frequency_map.has_key(word):
+			frequency_map[word] += 1
+		else:
+			frequency_map[word] = 1
+
+freq_list = sorted(frequency_map, key=frequency_map.get, reverse=True)
+
+for i in freq_list[:100]:
+	print "%s frequency: %s occurences" % (i, str(frequency_map[i]))
