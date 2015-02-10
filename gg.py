029605fdb9ba44a221535249ae6980512b2b3bd6
phouse512 <philiphouse2015@u.northwestern.edu>
2015-02-10 14:39:06 -0600

    adding time profiling

--- gg.py
+++ gg.py
@@ -1,29 +1,46 @@
 import urllib, json
 import collections
 import sys
+from profilehooks import profile
+import time
+
+start = time.clock()
 
 # gather filename
 filename = sys.argv[1]
 
 
 data = []
-with open(filename) as f:
-    for line in f:
-        data.append(json.loads(line))
-
 frequency_map = {}
-# array of tweets in data[0]
-for index, text in enumerate(data[0]):
-	print "Current Tweet %s: %s" % (index, text)
-	word_array = text['text'].split(" ")
-	for word in word_array:
 
-		if frequency_map.has_key(word):
-			frequency_map[word] += 1
-		else:
-			frequency_map[word] = 1
+@profile
+def load_json():
+	with open(filename) as f:
+	    for line in f:
+	        data.append(json.loads(line))
 
-freq_list = sorted(frequency_map, key=frequency_map.get, reverse=True)
+@profile
+def word_frequency():
+
+	# array of tweets in data[0]
+	for index, text in enumerate(data[0]):
+		#print "Current Tweet %s: %s" % (index, text)
+		word_array = text['text'].split(" ")
+		for word in word_array:
+			if frequency_map.has_key(word):
+				frequency_map[word] += 1
+			else:
+				frequency_map[word] = 1
+
+	return sorted(frequency_map, key=frequency_map.get, reverse=True)
+
+
+load_json()
+freq_list = word_frequency()
+
 
 for i in freq_list[:100]:
 	print "%s frequency: %s occurences" % (i, str(frequency_map[i]))
+
+end = time.clock()
+print "total time: %.2gs" % (end-start)
